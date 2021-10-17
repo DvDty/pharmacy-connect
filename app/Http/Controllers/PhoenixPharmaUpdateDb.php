7454b288e1f596e6d5594a851c20b22f32b67372
Pavel Grancharov <dvdtygc@gmail.com>
2021-10-17 23:07:17 +0300

    Add example product updater

---
+++ app/Http/Controllers/PhoenixPharmaUpdateDb.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Contracts\PhoenixPharmaClient;
+use App\Models\PhoenixPharmaProduct;
+
+class PhoenixPharmaUpdateDb extends Controller
+{
+    private const FETCH_PER_PAGE = 1000;
+
+    public function __invoke(PhoenixPharmaClient $client)
+    {
+        $pages = (int) ceil($client->productsCount() / self::FETCH_PER_PAGE);
+
+        foreach (range(0, $pages) as $page) {
+            $start = $page * self::FETCH_PER_PAGE;
+
+            $products = $client->products($start, $start + self::FETCH_PER_PAGE);
+
+            foreach ($products as $product) {
+                PhoenixPharmaProduct::updateOrCreate(
+                    $product->only('articleNumber')->all(),
+                    $product
+                        ->except('articleNumber')
+                        ->map(function ($product) {
+                            if (is_array($product)) {
+                                $product = '';
+                            }
+
+                            return $product;
+                        })
+                        ->all(),
+                );
+            }
+        }
+    }
+}
