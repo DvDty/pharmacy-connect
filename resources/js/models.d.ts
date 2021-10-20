0d0ceaa5052f514e5d00350015e1d1bacc588981
Pavel Grancharov <dvdtygc@gmail.com>
2021-10-20 03:24:50 +0300

    Publish updated model ts interfaces

--- resources/js/models.d.ts
+++ resources/js/models.d.ts
@@ -1,4 +1,46 @@
 declare namespace App.Models {
+    export interface Distributor {
+        id: number;
+        name: string;
+        updating: boolean;
+        last_updated: string | null;
+        products_class: string;
+        total_products: number;
+        created_at: string | null;
+        updated_at: string | null;
+    }
+    export interface PhoenixPharmaProduct {
+        id: number;
+        articleId: number;
+        articleNumber: number;
+        cyrName: string;
+        latName: string;
+        measureName: string;
+        producerCode: string;
+        producerName: string;
+        basePrice: number;
+        salePrice: number;
+        maxPrice: number;
+        nhifCode: string;
+        nhifBasePrice: number;
+        nhifSalePrice: number;
+        nhifMaxPrice: number;
+        isMedicalPrescription: boolean;
+        isWebSaleProhibition: boolean;
+        isDrugstoreAllowed: boolean;
+        isDrug: boolean;
+        isForRefrigerator: boolean;
+        advertismentText: string;
+        barcode1: string;
+        barcode2: string;
+        description: string;
+        overrateGroupID: number;
+        lastupdate: string;
+        isActive: boolean;
+        deleted: boolean;
+        created_at: string | null;
+        updated_at: string | null;
+    }
     export interface User {
         id: number;
         name: string;
